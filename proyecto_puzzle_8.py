c18974e39aa7859a5b4348bb8f526898826425fe
Marianne <mariannejmb18@gmail.com>
2021-04-08 10:38:23 -0400

    bfs Marianne

--- proyecto_puzzle_8.py
+++ proyecto_puzzle_8.py
@@ -30,7 +30,28 @@ MaxFrontier = 0
 
 def bfs_search(initial_state):
     'CODE'
-            
+    global MaxFrontier, GoalNode, MaxSearchDeep
+
+    boardVisited = set()
+    Queue = deque([PuzzleState(initial_state, None, None, 0, 0, 0)])
+
+    while Queue:
+        node = Queue.popleft()
+        boardVisited.add(node.map)
+        if node.state == GoalState:
+            GoalNode = node
+            return Queue
+        posiblePaths = subNodes(node)
+        for path in posiblePaths:
+            if path.map not in boardVisited:
+                Queue.append(path)
+                boardVisited.add(path.map)
+                if path.depth > MaxSearchDeep:
+                    MaxSearchDeep = MaxSearchDeep + 1
+        if len(Queue) > MaxFrontier:
+            QueueSize = len(Queue)
+            MaxFrontier = QueueSize
+
 def dfs_search(initial_state):
     'CODE'
 
